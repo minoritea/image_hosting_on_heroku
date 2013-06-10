5086b512eaaacd07cc983d202c4d909c26d02ae5
minoritea <m.tokuda@aol.jp>
2013-06-10 20:46:44 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ImageHosting::Application.config.secret_token = '0ee0d64388e4d75b4ae306be6ff3cfc945bc414a8a7dd911a5aebfe2c7c26a1ed726d49e57b5c48b7072c8b01173510e543e7086c8ba225af97f6f4a75c98978'
